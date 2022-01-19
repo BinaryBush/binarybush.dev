bef86d345a561af3767f347ee463fdbb924d9495
Tamás Csertán <33261144+csertant@users.noreply.github.com>
2022-01-20 00:12:03 +0100

    init: add initial files

---
+++ main.js
@@ -0,0 +1,4 @@
+/*Copyright 2022 binarybush.dev - All rights reserved.
+This work was created and is maintained by Tamas Csertan and Mark Sarvari (binarybush.dev).
+See the LICENSE file for terms of use.
+*/
